2095c2f1736cb69f3fa5a909db46386df5a52e1f
nuidom <nuidom@hotmail.com>
2021-08-26 00:09:43 +0100

    Generalized CLK_CONFIG_Init an UARTS_MODE_Init

--- max14830.c
+++ max14830.c
@@ -51,38 +51,44 @@
 //Board Revision
 #define MAX14830_REVID          0x25
 
-void CLK_Init();
-void UARTS_Mode_Init();
+void CLK_CONFIG_Init(uint8_t UART);
+void UARTS_Mode_Init(uint8_t UART);
 void MAX_Initialize()
 {
     int x=0;
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_MODE2, 0x01);
 //    for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_MODE2, 0x00);
-    CLK_Init();
-    UARTS_Mode_Init();
+    CLK_CONFIG_Init(MAX_UART0_WRITE);
+    CLK_CONFIG_Init(MAX_UART1_WRITE);
+    CLK_CONFIG_Init(MAX_UART2_WRITE);
+    CLK_CONFIG_Init(MAX_UART3_WRITE);
+    UARTS_Mode_Init(MAX_UART0_WRITE);
+    UARTS_Mode_Init(MAX_UART1_WRITE);
+    UARTS_Mode_Init(MAX_UART2_WRITE);
+    UARTS_Mode_Init(MAX_UART3_WRITE);
     maxReadByte(MAX_UART0_WRITE, MAX_UART0_READ, MAX14830_FIFOTRGLVL);        //set
     
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
 }
 
-void CLK_Init()
+void CLK_CONFIG_Init(uint8_t UART)
 {
-    maxWriteByte(MAX_UART0_WRITE, MAX14830_PLLCONFIG , 0x01);        //set PllConfig reg
+    maxWriteByte(MAX_UART0_WRITE, MAX14830_PLLCONFIG , 0x01);        // PreDIV0=1
     int x=0;
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
-    maxWriteByte(MAX_UART0_WRITE, MAX14830_BRGCONFIG, 0x00);        //set BEGConfig reg
+    maxWriteByte(UART, MAX14830_BRGCONFIG, 0x00);         //
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
-    maxWriteByte(MAX_UART0_WRITE, MAX14830_DIVLSB, 0x02);        //set
+    maxWriteByte(UART, MAX14830_DIVLSB, 0x02);           // Div1=1
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
-    maxWriteByte(MAX_UART0_WRITE, MAX14830_DIVMSB, 0x00);        //set
+    maxWriteByte(UART, MAX14830_DIVMSB, 0x00);           //
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
-    maxWriteByte(MAX_UART0_WRITE, MAX14830_CLKSOURCE, 0x0A);        //set CLKSource reg
+    maxWriteByte(MAX_UART0_WRITE, MAX14830_CLKSOURCE, 0x0A);        // PLLBypass=1, CystalEn=1
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
 
 }
 
-void UARTS_Mode_Init()
+void UARTS_Mode_Init(uint8_t UART)
 {
     maxWriteByte(MAX_UART0_WRITE, MAX14830_MODE1 , 0x00);
     int x=0;
@@ -91,8 +97,8 @@ void UARTS_Mode_Init()
 //    for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_MODE2 , 0x00);
     for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
-    maxWriteByte(MAX_UART0_WRITE, MAX14830_LCR , 0x03); // LCR setup 8 bit word, 1 stop bit, no parity
-    for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
+    maxWriteByte(UART, MAX14830_LCR , 0x03);             // LCR setup 8 bit word, 1 stop bit, no parity
+    for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}             //StopBits=1, Length0=1
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_RXTIMEOUT , 0x00);
 //    for(x=0;x<1000;x++){int y=0; for(y=0;y<500;y++){;}}
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_HDPLXDELAY , 0x00);
