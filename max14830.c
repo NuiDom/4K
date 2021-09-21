cda44e57b557aac90a2bc01b9521036c627ece75
nuidom <nuidom@hotmail.com>
2021-09-21 16:40:43 +0100

    interrupt set for POWER_BUTTON_STAT GPIO

--- max14830.c
+++ max14830.c
@@ -4,6 +4,7 @@
 void CLK_CONFIG_Init(uint8_t UART);
 void UARTS_Mode_Init(uint8_t UART);
 void GPIO_Init();
+void INTERRUPT_Init();
 
 void MAX_Init()
 {
@@ -17,7 +18,9 @@ void MAX_Init()
     UARTS_Mode_Init(MAX_UART2_WRITE);
     UARTS_Mode_Init(MAX_UART3_WRITE);
     
-    GPIO_Init();        
+    GPIO_Init();
+    
+    INTERRUPT_Init();
 }
 
 void CLK_CONFIG_Init(uint8_t UART)
@@ -31,20 +34,13 @@ void CLK_CONFIG_Init(uint8_t UART)
 
 void UARTS_Mode_Init(uint8_t UART)
 {
-//    maxWriteByte(UART, MAX14830_MODE1 , 0x00);
-//    delay_ms(200);
+    maxWriteByte(UART, MAX14830_MODE1 , 0x80);
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_MODE2 , 0x02);
-//    delay_ms(200);
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_MODE2 , 0x00);
-//    delay_ms(200);
     maxWriteByte(UART, MAX14830_LCR , 0x03);                        //LCR setup 8 bit word, 1 stop bit, no parity
-//    delay_ms(200);                                                     //StopBits=1, Length0=1
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_RXTIMEOUT , 0x00);
-//    delay_ms(200);
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_HDPLXDELAY , 0x00);
-//    delay_ms(200);
 //    maxWriteByte(MAX_UART0_WRITE, MAX14830_IRDA , 0x00);
-//    delay_ms(200);
 }
 
 void GPIO_Init()
@@ -52,4 +48,10 @@ void GPIO_Init()
     maxWriteByte(MAX_UART1_WRITE, MAX14830_GPIOCONFG, 0x08);        //GPIO7-OUT
     maxWriteByte(MAX_UART2_WRITE, MAX14830_GPIOCONFG, 0x0B);        //GPIO8-O, GPIO9-O, GPIO10-I, GPIO11-O
     maxWriteByte(MAX_UART3_WRITE, MAX14830_GPIOCONFG, 0x0E);        //GPIO13-0, GPIO14-O, GPIO15-O
+}
+
+void INTERRUPT_Init()
+{
+    maxWriteByte(MAX_UART2_WRITE, MAX14830_IRQEN, 0x04);
+    maxWriteByte(MAX_UART2_WRITE, MAX14830_STSINTEN, 0x04);     //enable interrupt for POWER_BUTTON_STAT GPIO
 }
